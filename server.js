3358f582c6429c87e43d16842b8e5b1a22d53a9d
Cesar Rodriguez <cesarrodriguez@MacBook-Pro-de-Cesar.local>
2025-08-17 17:31:42 -0300

    Express setups

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.user(express.json());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {  userNewUrlParser: true, userUnifiedTopology: true })
     .then(() => console.log("Mongo Connected"))
     .catch(err => console.log(err));
 
 app.get("/api/hello", (req, res) => {
-    req.json({ message: "Hello from Express" });
+    res.json({ message: "Hello from Express" });
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port ${PORT}"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
